b14bb2c762d11a901f752107dd3d03c9ffa70e83
manish vishwakarma <prakashvish1973@gmail.com>
2025-08-24 20:06:49 +0530

    Add Clerk Authentication

---
+++ components/Navbar.tsx
@@ -0,0 +1,14 @@
+import { checkUser } from '@/lib/checkUser';
+
+function Navbar() {
+
+  const user = checkUser();
+
+  return (
+    <div>
+      Navbar
+    </div>
+  )
+}
+
+export default Navbar;
